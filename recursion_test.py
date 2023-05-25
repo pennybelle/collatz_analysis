f9a9250749c4bfef2beb632aeea24ffee25bc83d
cake <79643779+mxratt@users.noreply.github.com>
2023-05-25 19:34:56 -0400

    collatz recursion test
    added comments and some small changes

--- recursion_test.py
+++ recursion_test.py
@@ -1,21 +1,22 @@
-def collatz(x=300339047, step=0):
-    if step > 993:
-        print('step exceeded recursion depth of 995')
+def collatz(x=300339047, step=0): # x default is highest seed b4 stack overflow
+    if step > 993: # prevents error when stack is abt to overflow
+        print(f'{x} exceeded recursion depth of 995')
         exit()
-    elif x == 1:
+    elif x == 1: # ends the recursion, returns the score (how many steps)
         return step
-    elif x % 2 == 0:
+    elif x % 2 == 0: # if even, divide by 2
         return collatz(x // 2, step + 1)
-    else:
+    else: # if odd, multiply by 3 and add 1
         return collatz(3 * x + 1, step + 1)
 
-def run_collatz(x):
-    most_steps = 0
-    while True:
-        steps = collatz(x)
-        if steps > most_steps:
+def run_collatz(x): # run the conjecture in a loop
+    most_steps = 0 # keeps score of most steps taken to get from x to 1
+    while True: # infinite loop to continuously test
+        steps = collatz(x) # runs the conjecture recursion function
+        if steps > most_steps: # sets new score if higher than previous
             most_steps = steps
-        print(f'{x}: {steps}\tsteps | most steps taken = {most_steps}')
-        x += 1
+        print(f'{x}: {steps}\tsteps   |   most steps taken = {most_steps}')
+        x += 1 # move to next integer to test
     
-run_collatz(1)
+run_collatz(1) # run the infinite test loop
+# x: steps
