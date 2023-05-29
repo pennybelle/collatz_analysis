9fde5de8f07d30607b79e21e2b7bf6c7e7b58b98
cake <79643779+mxratt@users.noreply.github.com>
2023-05-28 22:13:39 -0400

    recursion.py 1.01
    fixed the scroll spam, the output now overwrites itself so it stays on one line, and removed x's default value as its a pointless default

--- recursion_test.py
+++ recursion_test.py
@@ -1,6 +1,9 @@
-def collatz(x=300339047, step=0): # x default is highest seed b4 stack overflow
-    if step > 993: # prevents error when stack is abt to overflow
-        print(f'{x} exceeded recursion depth of 995')
+import sys
+sys.setrecursionlimit(10000)
+
+def collatz(x, step=0):
+    if step > 4993: # prevents error when stack is abt to overflow
+        print(f'{x} exceeded recursion depth of 5000')
         exit()
     elif x == 1: # ends the recursion, returns the score (how many steps)
         return step
@@ -11,11 +14,13 @@ def collatz(x=300339047, step=0): # x default is highest seed b4 stack overflow
 
 def run_collatz(x): # run the conjecture in a loop
     most_steps = 0 # keeps score of most steps taken to get from x to 1
+    best_seed = 0
     while True: # infinite loop to continuously test
         steps = collatz(x) # runs the conjecture recursion function
         if steps > most_steps: # sets new score if higher than previous
             most_steps = steps
-        print(f'{x}: {steps}\tsteps   |   most steps taken = {most_steps}')
+            best_seed = x
+        print(f' {x}: {steps}\tsteps  |  most steps taken = {most_steps}  |  best seed = {best_seed}', end='\r')
         x += 1 # move to next integer to test
     
 run_collatz(1) # run the infinite test loop
